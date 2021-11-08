ad1e1dbf5701c551b4e2990cb6959d36b87058db
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2021-11-08 23:14:36 +0630

    #math 1000

---
+++ youngphysicist.cpp
@@ -0,0 +1,75 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+// long long
+typedef long long Long;
+typedef long double BFloat;
+// vector strings
+typedef vector<string> Vstring; typedef vector<Vstring> VVString;
+// vector primitives
+typedef vector<char> Vchar; typedef vector<Vchar> VVchar;
+typedef vector<int> Vint; typedef vector<Vint> VVint;
+typedef vector<Long> Vlong; typedef vector<Vint> VVlong;
+
+// declare var and immediately cin
+#define cinvar(type, name)\
+	type name; cin >> name;
+
+#define cinchar(name) cinvar(char, name)
+#define cinuint(name) cinvar(unsigned int, name)
+#define cinint(name) cinvar(int, name)
+#define cinlong(var) cinvar(long long, var)
+#define cinulong(var) cinvar(unsigned long long, var)
+#define cindouble(var) cinvar(double, var)
+#define cinstring(var) cinvar(string, var)
+// declare var and immediately getline
+#define cinline(var)\
+	string var; getline(cin, var);
+
+#define cinvector(type, name, n) \
+	vector<type> name(n);\
+	for(auto& e: name) cin >> e;
+
+
+#define twhile(a)\
+	int a; cin >> a; while(a--)
+#define tfor(i, a)\
+	int a; cin >> a; for(int i = 0; i < a; i++)
+
+#define f(i, a, n) for(int i = a; i < n; i++)
+#define fe(i, a, n) for(int i = a; i <= n; i++)
+#define fr(i, a, n) for(int i = n-1; i> a; i--)
+#define fer(i, a, n) for(int i = n-1; i >= a; i--)
+
+#define fstr(i, s) f(i, 0, s.length())
+#define ferstr(i, s) fr(i, s.length()-1, 0)
+
+#define pvc(target, v, c)\
+	for(int i = 0; i < v.size(); i++)\
+		target << v[i] << (i==v.size()-1 ? '\n': c);
+
+#define pv(v)\
+	pvc(cout, v, ' ')
+
+// ---------------------
+
+bool isvowel(char c) {
+	switch(tolower(c)) {
+		case 'a': case 'y': case 'e': case 'i': case 'o': case 'u': return true;
+		default: return false;
+	}
+}
+
+int main() {
+	int x = 0, y = 0, z = 0;
+	twhile(t) {
+		cinint(xx); cinint(yy); cinint(zz);
+		x+=xx; y+=yy; z+=zz;
+	}
+
+	cout << ((x or y or z) ? "NO" : "YES") << endl;
+	
+	return 0;
+}
+

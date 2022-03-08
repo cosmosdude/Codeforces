88b9cd1b09c4c524b34e57b8f18c491ad4cb3fe3
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2022-03-09 00:09:51 +0630

    Div 3 Problem A

---
+++ src/deletion_of_two_adjacent_letters.cpp
@@ -0,0 +1,89 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+// long long
+typedef long long Long;
+typedef long double BFloat;
+// vector strings
+typedef vector<string> Vstring;
+// vector primitives
+typedef vector<char> Vchar; typedef vector<Vchar> VVchar;
+typedef vector<int> Vint; typedef vector<Vint> VVint;
+typedef vector<Long> Vlong; typedef vector<Vint> VVlong;
+
+// declare var and immediately cin
+#define cinvar(type, name)\
+	type name; cin >> name;
+
+#define cinchar(name) cinvar(char, name)
+#define cinuint(name) cinvar(unsigned int, name)
+#define cinint(name) cinvar(int, name)
+#define cinlong(var) cinvar(long long, var)
+#define cinulong(var) cinvar(unsigned long long, var)
+#define cindouble(var) cinvar(double, var)
+#define cinstring(var) cinvar(string, var)
+// declare var and immediately getline
+#define cinline(var)\
+	string var; getline(cin, var);
+
+#define cinvector(type, name, n) \
+	vector<type> name(n);\
+	for(auto& e: name) cin >> e;
+
+
+#define twhile(a)\
+	int a; cin >> a; cin.ignore(); while(a--)
+#define tfor(i, a)\
+	int a; cin >> a; cin.ignore(); for(int i = 0; i < a; i++)
+
+// quick for
+#define f(i, a, n) for(int i = a; i < n; i++)
+#define fe(i, a, n) for(int i = a; i <= n; i++)
+#define fr(i, a, n) for(int i = n-1; i> a; i--)
+#define fer(i, a, n) for(int i = n-1; i >= a; i--)
+
+#define fstr(i, s) f(i, 0, s.length())
+#define ferstr(i, s) fr(i, s.length()-1, 0)
+
+#define pvc(target, v, c)\
+	for(int i = 0; i < v.size(); i++)\
+		target << v[i] << (i==v.size()-1 ? '\n': c);
+
+#define pv(v)\
+	pvc(cout, v, ' ')
+
+//----------------------
+
+int main() {
+
+	cinint(n);
+	while(n--) {
+		cinstring(str);
+		cinchar(c);	
+
+		vector<int> indices;
+		for(int i = 0; i < str.length(); i++) {
+			if (str[i] == c) indices.push_back(i);
+		}
+
+		bool status = false;
+		for(int each: indices) {
+			if (each == 0 or each == str.length()-1) {
+				status = (str.length()-1)%2 == 0;
+				if (status) break;
+			}
+
+			int first = each;
+			int second = str.length() - (each+1);
+			status = (first%2 == 0) and (second%2 == 0);
+			if (status) break;
+		}
+
+		cout << (status?"YES":"NO") << endl;
+	}
+	
+
+	return 0;
+}
+

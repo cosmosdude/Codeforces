c3663f1a88cb04d311405d1ed9880c9d98afc036
Thwin Htoo Aung <thwinhtooaungreborn@gmail.com>
2022-03-09 00:06:44 +0630

    Twist The Permutation Div3

---
+++ src/twist_the_permutation.cpp
@@ -0,0 +1,102 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+// long long
+typedef long long Long;
+typedef long double BFloat;
+// vector strings
+typedef vector<string> Vstring;
+// vector primitives
+typedef vector<char> Vchar; typedef vector<Vchar> VVchar;
+typedef vector<int> Vint; typedef vector<Vint> VVint;
+typedef vector<Long> Vlong; typedef vector<Vint> VVlong;
+
+// declare var and immediately cin
+#define cinvar(type, name)\
+	type name; cin >> name;
+
+#define cinchar(name) cinvar(char, name)
+#define cinuint(name) cinvar(unsigned int, name)
+#define cinint(name) cinvar(int, name)
+#define cinlong(var) cinvar(long long, var)
+#define cinulong(var) cinvar(unsigned long long, var)
+#define cindouble(var) cinvar(double, var)
+#define cinstring(var) cinvar(string, var)
+// declare var and immediately getline
+#define cinline(var)\
+	string var; getline(cin, var);
+
+#define cinvector(type, name, n) \
+	vector<type> name(n);\
+	for(auto& e: name) cin >> e;
+
+
+#define twhile(a)\
+	int a; cin >> a; cin.ignore(); while(a--)
+#define tfor(i, a)\
+	int a; cin >> a; cin.ignore(); for(int i = 0; i < a; i++)
+
+// quick for
+#define f(i, a, n) for(int i = a; i < n; i++)
+#define fe(i, a, n) for(int i = a; i <= n; i++)
+#define fr(i, a, n) for(int i = n-1; i> a; i--)
+#define fer(i, a, n) for(int i = n-1; i >= a; i--)
+
+#define fstr(i, s) f(i, 0, s.length())
+#define ferstr(i, s) fr(i, s.length()-1, 0)
+
+#define pvc(target, v, c)\
+	for(int i = 0; i < v.size(); i++)\
+		target << v[i] << (i==v.size()-1 ? '\n': c);
+
+#define pv(v)\
+	pvc(cout, v, ' ')
+
+//----------------------
+
+void solve() {
+	cinint(n);
+	cinvector(int, a, n);
+
+	vector<int> pos(n+1);
+
+	for(int i = 0; i < n; i++)
+		pos[a[i]] = i+1;
+
+	vector<int> moves;
+
+	int cap = n;
+
+	for(int i = n; i>0; i--) {
+
+		int p = pos[i];
+
+		int move = 0;
+
+		if (p != i) move = p;
+
+		moves.push_back(move);
+
+		for(int j = 0; j < i; j++) {
+			pos[j] -= move;
+			if (pos[j] < 1) pos[j] += cap;
+		}
+
+		cap --;
+	}
+
+	reverse(moves.begin(), moves.end());
+	pv(moves);
+}
+
+
+int main() {
+
+	cinint(n);
+
+	while(n--) solve();
+	
+	return 0;
+}
+
